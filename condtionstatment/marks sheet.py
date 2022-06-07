0436852723c2dda7f377bc7557e4153f815b33f2
dhirazz90 <dhirazz90@gmail.com>
2022-06-07 18:48:07 +0545

    update

--- condtionstatment/marks sheet.py
+++ condtionstatment/marks sheet.py
@@ -1,18 +1,24 @@
-nepali= int(input("nepali marks "))
-maths = int(input("maths marks "))
-science = int(input("science marks "))
-social = int(input("social marks "))
-total = (nepali + maths  + science + social)
-print(total)
-percentage = (total/4)
-print(percentage)
-if percentage > 35 and percentage <= 45:
-    print("third")
-elif percentage > 45 and percentage <= 60:
-        print("second")
-elif percentage > 60 and percentage <= 80:
-            print("first")
-elif percentage > 80 and percentage <= 100:
-                print("top")
+nepali= int(input("Enter Nepali marks "))
+maths = int(input("Enter Maths marks "))
+science = int(input("Enter Science marks "))
+social = int(input("Enter Social marks "))
+HPE = int(input("Enter HPE marks "))
+Opt_maths = int(input("Enter Opt.Maths marks "))
+coputer = int(input("Enter Computer marks "))
+english = int(input("Enter English marks "))
+total = (nepali + maths  + science + social + HPE + Opt_maths + coputer + english)
+print("total marks: ",total)
+percentage = (total/8)
+print("obtained percentage: ",percentage)
+if percentage > 50 and percentage <= 60:
+    print("Grade: C+")
+elif percentage > 60 and percentage <= 70:
+    print("Grade: B")
+elif percentage > 70 and percentage <= 80:
+        print("Grade: B+")
+elif percentage > 80 and percentage <= 90:
+            print("Grade: A")
+elif percentage > 90 and percentage <= 100:
+                print("GPA: A+")
 else:
-            print("fail")
+            print("FAIL")
